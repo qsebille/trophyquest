d4fabcd24abb7e146befa6b75d0a5ae259e48aba
Quentin Sebille <quentin.sebille@gmail.com>
2026-02-10 14:53:58 +0100

    Initialisation of image-uploader lambda

--- lambda/python/src/images/database/queries/select_images.py
+++ lambda/python/src/images/database/queries/select_images.py
@@ -1,8 +1,9 @@
 def select_psn_game_images(nb_image_to_select, connection):
     cursor = connection.cursor()
     query = f"""
-            select pgi.id, pgi.psn_url
+            select pgi.id, pgi.psn_url, pgi.type, pg.name, pg.id 
                 from app.psn_game_image pgi
+                    left join app.psn_game pg on pg.id = pgi.psn_game_id
                 where pgi.aws_url is null
             limit {nb_image_to_select};
             """
