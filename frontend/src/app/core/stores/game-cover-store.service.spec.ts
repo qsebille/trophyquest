8e67d76d3a041e329fe4fcff4ecc4d53993fe747
Quentin Sebille <quentin.sebille@gmail.com>
2026-02-11 15:39:03 +0100

    Fix frontend tests

--- frontend/src/app/core/stores/game-cover-store.service.spec.ts
+++ frontend/src/app/core/stores/game-cover-store.service.spec.ts
@@ -1,16 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { GameCoverStoreService } from './game-cover-store.service';
+import {GameCoverStoreService} from './game-cover-store.service';
+import {GameApiService} from "../api/services/game-api.service";
 
 describe('GameCoverStoreService', () => {
-  let service: GameCoverStoreService;
+    let service: GameCoverStoreService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GameCoverStoreService);
-  });
+    let gameApiServiceSpy: jasmine.SpyObj<GameApiService>;
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        gameApiServiceSpy = jasmine.createSpyObj('GameApiService', ['fetchRandomCoverImage']);
+        TestBed.configureTestingModule({
+            providers: [{provide: GameApiService, useValue: gameApiServiceSpy}]
+        });
+        service = TestBed.inject(GameCoverStoreService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
