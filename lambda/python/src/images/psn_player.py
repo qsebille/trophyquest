0386b7c2ef8c69669d281fa382c154ea64cfb32e
Quentin Sebille <quentin.sebille@gmail.com>
2026-02-28 09:45:14 +0100

    Fix PSN Player query in image upload

--- lambda/python/src/images/psn_player.py
+++ lambda/python/src/images/psn_player.py
@@ -11,7 +11,7 @@ def select_player_image(limit: int, connection):
     query = f"""
             SELECT p.id, p.psn_avatar_url, p.pseudo 
                 FROM app.psn_player p
-                WHERE p.aws_url IS NULL
+                WHERE p.psn_avatar_url IS NULL
             LIMIT {limit};
             """
 
