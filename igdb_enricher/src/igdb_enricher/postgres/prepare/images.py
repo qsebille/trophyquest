fbf953c41fa91023c49dc3dc1c8063c30fc59c39
Quentin Sebille <quentin.sebille@gmail.com>
2026-03-21 13:38:05 +0100

    Fix prepare images

--- igdb_enricher/src/igdb_enricher/postgres/prepare/images.py
+++ igdb_enricher/src/igdb_enricher/postgres/prepare/images.py
@@ -9,7 +9,7 @@ def prepare_images(processed_candidates: list[CandidateResultingProcess]):
 
     for game in [c for process_output in processed_candidates for c in process_output.igdb_game_list]:
         images.extend(
-            {'igdb_id': game.get("id"), 'url': image.get("url"), 'image_type': image.get("type")}
+            (game.get("id"), image.get("url"), image.get("type"))
             for image in game.get("images", [])
         )
 
