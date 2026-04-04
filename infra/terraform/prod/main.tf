3f0da0b41e9d13869e36ca271a29a842fe6011d9
Quentin Sebille <quentin.sebille@gmail.com>
2026-04-04 17:52:10 +0200

    Added terraform state S3

--- infra/terraform/prod/main.tf
+++ infra/terraform/prod/main.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.8.0"
 
+  backend "s3" {
+    bucket       = "trophyquest-terraform-state"
+    key          = "prod/terraform.tfstate"
+    region       = "eu-west-3"
+    use_lockfile = true
+    encrypt      = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -16,6 +24,6 @@ provider "aws" {
 module "static_frontend_s3" {
   source = "../modules/static_frontend_s3"
 
-  bucket_name = var.frontend_bucket_name
+  bucket_name  = var.frontend_bucket_name
   project_name = var.project_name
 }
