4cdc073cdc03c482537b3a6413741740bd811b22
qsebille <quentin.sebille@gmail.com>
2026-04-04 17:53:04 +0200

    Terraform init (#79)
    * Added infra terraform : static S3 frontend
    
    * Added terraform state S3

---
+++ infra/terraform/prod/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.8.0"
+
+  backend "s3" {
+    bucket       = "trophyquest-terraform-state"
+    key          = "prod/terraform.tfstate"
+    region       = "eu-west-3"
+    use_lockfile = true
+    encrypt      = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "static_frontend_s3" {
+  source = "../modules/static_frontend_s3"
+
+  bucket_name  = var.frontend_bucket_name
+  project_name = var.project_name
+}
---
+++ infra/terraform/prod/terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region           = "eu-west-3"
+project_name         = "trophyquest"
+frontend_bucket_name = "trophyquest-webapp"
